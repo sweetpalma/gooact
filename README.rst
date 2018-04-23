9edaa99c83f27b06048ad5ac932bd5d04b1ac811
SweetPalma <sweet.palma@yandex.ru>
2018-04-23 13:17:04 +0300

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,31 @@
+======
+Gooact
+======
+
+    Copyright 2017 SweetPalma <sweet.palma@yandex.ru>
+
+Own React in 160 lines of JavaScript.
+
+Introduction
+============
+React is a great library — many developers instantly fell in love with it due to simplicity, performance and declarative way of doing things. But personally I have a specific reason what makes it so special for me — and that’s how it works under the hood. I find ideas that stand behind React simple yet strangely fascinating — and I believe that understanding its core principles would help you writing faster and safer code.
+
+So I write a Medium article where how to write a fully functional React clone, including Component API and own Virtual DOM implementation. This is a public repository of Gooact itself.
+
+Installation & Usage
+====================
+This is a standalone version of Gooact - it can be installed with NPM:
+
+.. code-block:: 
+	
+	npm install sweetpalma/gooact
+
+And used just as a regular JavaScript library:
+
+.. code-block:: javascript
+
+	import {render, Component} from 'gooact';
+
+License
+=======
+Gooact is licensed under the MIT License, what allows you to use it for basically anything absolutely for free. It would be great if somebody will find it useful.
---
+++ gooact.js
@@ -0,0 +1,160 @@
+/* Gooact by SweetPalma, 2018. All rights reserved. */
+((exports) => { 'use strict';
+
+const createElement = exports.createElement = (type, props, ...children) => {
+    props = props != null ? props : {};
+    return {type, props, children};
+};
+
+const setAttribute = exports.setAttribute = (dom, key, value) => {
+    if (typeof value == 'function' && key.startsWith('on')) {
+        const eventType = key.slice(2).toLowerCase();
+        dom.__gooactHandlers = dom.__gooactHandlers || {};
+        dom.removeEventListener(eventType, dom.__gooactHandlers[eventType]);
+        dom.__gooactHandlers[eventType] = value;
+        dom.addEventListener(eventType, dom.__gooactHandlers[eventType]);
+    } else if (key == 'checked' || key == 'value' || key == 'id') {
+        dom[key] = value;
+    } else if (key == 'key') {
+        dom.__gooactKey = value;
+    } else if (typeof value != 'object' && typeof value != 'function') {
+        dom.setAttribute(key, value);
+    }
+};
+
+const render = exports.render = (vdom, parent=null) => {
+    if (parent) parent.textContent = '';
+    const mount = parent ? (el => parent.appendChild(el)) : (el => el);
+    if (typeof vdom == 'string' || typeof vdom == 'number') {
+        return mount(document.createTextNode(vdom));
+    } else if (typeof vdom == 'boolean' || vdom === null) {
+        return mount(document.createTextNode(''));
+    } else if (typeof vdom == 'object' && typeof vdom.type == 'function') {
+        return mount(Component.render(vdom));
+    } else if (typeof vdom == 'object' && typeof vdom.type == 'string') {
+        const dom = document.createElement(vdom.type);
+        for (const child of [/* flatten */].concat(...vdom.children))
+            dom.appendChild(render(child));
+        for (const prop in vdom.props)
+            setAttribute(dom, prop, vdom.props[prop]);
+        return mount(dom);
+    } else {
+        throw new Error(`Invalid VDOM: ${vdom}.`);
+    }
+};
+
+const patch = exports.patch = (dom, vdom, parent=dom.parentNode) => {
+    const replace = parent ? el => (parent.replaceChild(el, dom) && el) : (el => el);
+    if (typeof vdom == 'object' && typeof vdom.type == 'function') {
+        return Component.patch(dom, vdom, parent);
+    } else if (typeof vdom != 'object' && dom instanceof Text) {
+        return dom.textContent != vdom ? replace(render(vdom)) : dom;
+    } else if (typeof vdom == 'object' && dom instanceof Text) {
+        return replace(render(vdom));
+    } else if (typeof vdom == 'object' && dom.nodeName != vdom.type.toUpperCase()) {
+        return replace(render(vdom));
+    } else if (typeof vdom == 'object' && dom.nodeName == vdom.type.toUpperCase()) {
+        const pool = {};
+        const active = document.activeElement;
+        for (const index in Array.from(dom.childNodes)) {
+            const child = dom.childNodes[index];
+            const key = child.__gooactKey || index;
+            pool[key] = child;
+        }
+        const vchildren = [/* flatten */].concat(...vdom.children);
+        for (const index in vchildren) {
+            const child = vchildren[index];
+            const key = child.props && child.props.key || index;
+            dom.appendChild(pool[key] ? patch(pool[key], child) : render(child));
+            delete pool[key];
+        }
+        for (const key in pool) {
+            if (pool[key].__gooactInstance)
+                pool[key].__gooactInstance.componentWillUnmount();
+            pool[key].remove();
+        }
+        for (const prop in vdom.props) {
+            setAttribute(dom, prop, vdom.props[prop]);
+        }
+        active.focus();
+        return dom;
+    }
+};
+
+const Component = exports.Component = class Component {
+    constructor(props) {
+        this.props = props || {};
+        this.state = null;
+    }
+
+    static render(vdom, parent=null) {
+        const props = Object.assign({}, vdom.props, {children: vdom.children});
+        if (Component.isPrototypeOf(vdom.type)) {
+            const instance = new (vdom.type)(props);
+            instance.componentWillMount();
+            instance.base = render(instance.render(), parent);
+            instance.base.__gooactInstance = instance;
+            instance.base.__gooactKey = vdom.props.key;
+            instance.componentDidMount();
+            return instance.base;
+        } else {
+            return render(vdom.type(props), parent);
+        }
+    }
+
+    static patch(dom, vdom, parent=dom.parentNode) {
+        const props = Object.assign({}, vdom.props, {children: vdom.children});
+        if (dom.__gooactInstance.constructor == vdom.type) {
+            dom.__gooactInstance.componentWillReceiveProps(props);
+            dom.__gooactInstance.props = props;
+            return patch(dom, dom.__gooactInstance.render());
+        } else if (Component.isPrototypeOf(vdom.type)) {
+            const ndom = Component.render(vdom);
+            return parent ? (parent.replaceChild(ndom, dom) && ndom) : (ndom);
+        } else if (!Component.isPrototypeOf(vdom.type)) {
+            return patch(dom, vdom.type(props));
+        }
+    }
+
+    setState(nextState) {
+        if (this.base && this.shouldComponentUpdate(this.props, nextState)) {
+            const prevState = this.state;
+            this.componentWillUpdate(this.props, nextState);
+            this.state = nextState;
+            patch(this.base, this.render());
+            this.componentDidUpdate(this.props, prevState);
+        } else {
+            this.state = nextState;
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps != this.props || nextState != this.state;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        return undefined;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        return undefined;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        return undefined;
+    }
+
+    componentWillMount() {
+        return undefined;
+    }
+
+    componentDidMount() {
+        return undefined;
+    }
+
+    componentWillUnmount() {
+        return undefined;
+    }
+};
+
+})(typeof exports != 'undefined' ? exports : window.Gooact = {});
