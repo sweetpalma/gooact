3aa5fb879fa27c52b5afa350eb9f8dd988fb8dbb
Tuấn Vương <vanhtuan0409@gmail.com>
2018-05-31 09:56:52 +0700

    add example project

---
+++ example/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require("webpack");
+const fs = require("fs");
+const path = require("path");
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+module.exports = {
+  entry: resolveApp("./index.js"),
+  target: "web",
+  mode: process.env.WEBPACK_SERVE ? "development" : "production",
+  devtool: "source-map",
+
+  output: {
+    path: resolveApp("./public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
