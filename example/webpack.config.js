0f71ad718c76e8cded1d375fcda9fffa02c6140f
SweetPalma <sweet.palma@yandex.ru>
2018-06-14 23:20:18 +0300

    Slightly reworked example folder to comply with general style.

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -6,21 +6,20 @@ const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 module.exports = {
-  entry: resolveApp("./index.js"),
-  target: "web",
-  mode: process.env.WEBPACK_SERVE ? "development" : "production",
-  devtool: "source-map",
-
-  output: {
-    path: resolveApp("./public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js|jsx$/,
-        loader: "babel-loader"
-      }
-    ]
-  }
+    entry: resolveApp("./index.jsx"),
+    target: "web",
+    mode: process.env.WEBPACK_SERVE ? "development" : "production",
+    devtool: "source-map",
+    output: {
+        path: resolveApp("./public"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                loader: "babel-loader"
+            }
+        ]
+    }
 };
